b28bc349fa21fbe11d234ce709fd68a021736971
omarakhayyat <omar.a.khayyat@gmail.com>
2021-05-13 06:58:44 +0400

    Update settings.py
    Secret key removed

--- meeting_python_django/meeting_python_django/settings.py
+++ meeting_python_django/meeting_python_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p9rck_%==g&f0kmi_lfyrsrruvko8mwyx+d1_2iuzn1&(7oh0o'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
